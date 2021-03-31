abe0ad761a57b37e7aee246e69e5d856ba0db741
Lecturer <lecturer@wiut.uz>
2021-03-31 13:15:31 +0500

    some stuff

---
+++ README.md
@@ -0,0 +1,11 @@
+# Notes app
+
+## Front end workshop (finished)
+
+Feel free to change project files and use as a basis for your coursework
+
+## Back end workshop (week 1)
+
+To do:
+  - get user data (create form page) and save to file
+  - show saved data to the user (note list page)
---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+
+app.set('view engine', 'pug')
+
+// dev process
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+	res.render('home')
+})
+
+// localhost:8000/create
+app.get('/create', (req, res) => {
+	res.render('create')
+})
+
+
+
+app.post('/create', (req, res) => {
+	const title = req.body.title
+	const description = req.body.description
+
+	if (title.trim() === '' && description.trim() === '') {
+		res.render('create', { error: true })
+	} else {
+		fs.readFile('./data/notes.json', (err, data) => {
+			if (err) throw err
+
+			const notes = JSON.parse(data)
+
+			const newNote = {
+				id: id(),
+				title: title,
+				description: description 
+			}
+
+			notes.push(newNote)
+
+			fs.writeFile('./data/notes.json', JSON.stringify(notes), err => {
+				if (err) throw err
+
+				res.render('create', { success: true })
+			})
+		})
+	}
+})
+
+
+
+
+app.get('/notes', (req, res) => {
+	fs.readFile('./data/notes.json', (err, data) => {
+		if (err) throw err
+
+		const notes = JSON.parse(data)
+
+		res.render('notes', { notes: notes })
+	})
+})
+
+
+
+app.get('/notes/detail', (req, res) => {
+	res.render('detail')
+})
+
+
+
+app.listen(8000, err => {
+	if(err) throw err
+
+	console.log('App is running on port 8000...')
+})
+
+
+function id () {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
